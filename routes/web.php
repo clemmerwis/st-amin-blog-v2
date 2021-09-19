69e079ad89c13da9b5961dfbcc69325824b1bd4c
Christopher Lemmer <lemmer.j.christopher@gmail.com>
2021-09-19 00:43:39 -0500

    Add laravel ui && Add autoprefix to webpack mix

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
