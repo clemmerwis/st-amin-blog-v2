8bb3ce681e0fce8a271bdf601ea75cdd4e3ed454
Christopher Lemmer <lemmer.j.christopher@gmail.com>
2022-01-16 00:59:13 -0600

    add post and comment migrations

---
+++ database/migrations/2022_01_16_041443_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->text('body');
+            $table->string('image_path')->nullable();
+            $table->string('slug')->unique();
+            $table->boolean('active')->default(true);
+            $table->boolean('featured')->default(false);
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')
+              ->references('id')->on('users')
+              ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
