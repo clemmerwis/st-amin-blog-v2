5dac4f5f5a0aa22f3af51a87a694d935d238f531
Christopher Lemmer <lemmer.j.christopher@gmail.com>
2022-07-11 21:14:15 -0500

    Install vuetify into project

--- resources/views/dashboards/admin.blade.php
+++ resources/views/dashboards/admin.blade.php
@@ -31,58 +31,61 @@
 </head>
 
 <body>
-    <div class="wrapper">
-        <x-admin.sidebar-nav />
+    <div id="app">
+        <v-app app>
+            <div class="wrapper">
+                <x-admin.sidebar-nav />
 
-        <div class="main">
-            <x-admin.topbar-nav />
+                <div class="main">
+                    <x-admin.topbar-nav />
 
-            <main class="content">
-                @if (!isset($active))
-                    <x-admin.innerpage />
-                @else
-                    @if ($active === 'Posts')
-                        <div id="app">
-                            <innerpage-posts></innerpage-posts>
-                        </div>
-                        {{-- <x-admin.innerpage-posts :posts="$posts"/> --}}
-                    {{-- @elseif ($active === 'Users')
-                        <x-admin.innerpage-users /> --}}
-                    @endif
-                @endif
-            </main>
+                    <main class="content">
+                        @if (!isset($active))
+                            <x-admin.innerpage />
+                        @else
+                            @if ($active === 'Posts')
 
-            <footer class="footer">
-                <div class="container-fluid">
-                    <div class="row text-muted">
-                        <div class="col-6 text-start">
-                            <p class="mb-0">
-                                <a class="text-muted" href="https://adminkit.io/"
-                                    target="_blank"><strong>AdminKit</strong></a> &copy;
-                            </p>
+                                    <innerpage-posts></innerpage-posts>
+
+                                {{-- <x-admin.innerpage-posts :posts="$posts"/> --}}
+                            {{-- @elseif ($active === 'Users')
+                                <x-admin.innerpage-users /> --}}
+                            @endif
+                        @endif
+                    </main>
+
+                    <footer class="footer">
+                        <div class="container-fluid">
+                            <div class="row text-muted">
+                                <div class="col-6 text-start">
+                                    <p class="mb-0">
+                                        <a class="text-muted" href="https://adminkit.io/"
+                                            target="_blank"><strong>AdminKit</strong></a> &copy;
+                                    </p>
+                                </div>
+                                <div class="col-6 text-end">
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item">
+                                            <a class="text-muted" href="https://adminkit.io/" target="_blank">Support</a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="text-muted" href="https://adminkit.io/" target="_blank">Help Center</a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="text-muted" href="https://adminkit.io/" target="_blank">Privacy</a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="text-muted" href="https://adminkit.io/" target="_blank">Terms</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-6 text-end">
-                            <ul class="list-inline">
-                                <li class="list-inline-item">
-                                    <a class="text-muted" href="https://adminkit.io/" target="_blank">Support</a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a class="text-muted" href="https://adminkit.io/" target="_blank">Help Center</a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a class="text-muted" href="https://adminkit.io/" target="_blank">Privacy</a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a class="text-muted" href="https://adminkit.io/" target="_blank">Terms</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                    </footer>
                 </div>
-            </footer>
-        </div>
+            </div>
+        </v-app>
     </div>
-
     @if (!isset($active))
         <x-admin.footerscripts />
     @else
