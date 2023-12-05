513a24b60452af0f32416b2a5f1d7f6b5da9c8d3
clemmer <lemmer.j.chris@gmail.com>
2023-12-04 18:03:06 -0600

    Add temporary favicon

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <link rel="icon" href="{{ asset('/img/hero/owlx400.png') }}" type="image/x-icon">
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
