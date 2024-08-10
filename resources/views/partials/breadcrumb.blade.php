39ef01d8d90c1b9e934f1aff0e2b72dab900e609
clemmer <lemmer.j.chris@gmail.com>
2024-08-09 20:20:19 -0500

    Remove links from breadcrumb section

--- resources/views/partials/breadcrumb.blade.php
+++ resources/views/partials/breadcrumb.blade.php
@@ -1,10 +1,6 @@
 <div class="breadcrumb-text">
     @if ($active == 'SoM')
         <h3>Stories of Mirrors: All Chapters</h3>
-        <div class="bt-option">
-            <a href="{{ route('home') }}">Home</a>
-            <span>Stories of Mirrors: Chapter Selection</span>
-        </div>
     @else
         @if(isset($category) && $category)
             <h3>{{ $categoryName }}</h3>
